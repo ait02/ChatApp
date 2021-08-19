23404d6dd5bdabe4bc8e633ee528317fffe31606
Arif Ikmal <arifikmal@gmail.com>
2021-08-19 23:17:59 +0800

    Single Chat Screen

---
+++ RN-ChatApp/src/screens/ChatScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const ChatScreen = () => {
+  return (
+    <View>
+      <Text>Single Chat</Text>
+    </View>
+  );
+};
+
+export default ChatScreen;
+
+const styles = StyleSheet.create({});
